1978fb4d2d2bf157b35e9d67888923b1880d22f9
irisdew <ycleardew@gmail.com>
2021-08-01 17:13:25 +0900

    Feat: 메인 페이지 UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Teacher from './components/Teacher';
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route exact path="/" component={Home} />
         <Route path="/teacher" component={Teacher} />
       </Switch>
     </Router>
